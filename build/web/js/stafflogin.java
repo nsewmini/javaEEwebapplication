a592586c4974a5af7cafd0ddacdce3930ab122e0
bmlcrsgunasekara <97513246+bmlcrsgunasekara@users.noreply.github.com>
2022-10-07 19:38:35 +0530

    Update stafflogin.java
    I edited the staff login

--- build/web/js/stafflogin.java
+++ build/web/js/stafflogin.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
 import java.io.IOException;
@@ -16,10 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import model.dbconnect;
 import model.loginbean;
 
-/**
- *
- * @author IS Rachinthana
- */
+
 public class stafflogin extends HttpServlet {
 
     /**
@@ -49,14 +42,7 @@ public class stafflogin extends HttpServlet {
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
