2aed9097996ab881a782aeadffe42ad1fca20b34
bmlcrsgunasekara <97513246+bmlcrsgunasekara@users.noreply.github.com>
2022-10-07 19:29:19 +0530

    Update userlogin.java
    I edited user login

--- userlogin.java
+++ userlogin.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
 import java.io.IOException;
@@ -16,21 +12,10 @@ import javax.servlet.http.HttpServletResponse;
 import model.dbconnect;
 import model.loginbean;
 
-/**
- *
- * @author IS Rachinthana
- */
+
 public class userlogin extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
