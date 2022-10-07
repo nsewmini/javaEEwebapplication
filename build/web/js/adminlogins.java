a1b3330a6efd9ce10af293c48c17ae68e61ae934
bmlcrsgunasekara <97513246+bmlcrsgunasekara@users.noreply.github.com>
2022-10-07 19:41:08 +0530

    Update adminlogins.java
    I edited admin login

--- build/web/js/adminlogins.java
+++ build/web/js/adminlogins.java
@@ -86,11 +86,7 @@ public class adminlogins extends HttpServlet {
         }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+   
     @Override
     public String getServletInfo() {
         return "Short description";
